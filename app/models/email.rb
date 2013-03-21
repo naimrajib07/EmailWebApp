6ef3dffa5e42a8a3f65bbb067e13411649fb5957
Pandu Ramesh <bhanurorkitchen8@gmail.com>
2013-03-21 16:32:20 +0600

    - Added email attachement accesible .

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,7 +3,7 @@ class Email < ActiveRecord::Base
   has_many :email_lists, :dependent => :destroy
   belongs_to :user
 
-  attr_accessible :email_body, :email_from, :email_subject, :email_to, :sender_id, :bcc, :email_attachment
+  attr_accessible :email_body, :email_from, :email_subject, :email_to, :sender_id, :bcc, :email_attachment, :email_attachment_file_name
 
   attr_accessor :bcc
 
