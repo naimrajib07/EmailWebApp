84eaec19c17744cf054e9c720965521106e0ab42
Pandu Ramesh <bhanurorkitchen8@gmail.com>
2013-03-22 23:45:45 +0600

    - Added traderline.pt smtp setting for mail    configuration.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,14 @@
 require "development_mail_interceptor"
 
 ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "www.nascenia.com",
-    :user_name => "do-not-reply@nascenia.com",
-    :password => "12nascenia3",
-    :authentication => "plain"
+    :address => "traderline.pt",
+    :port => 25,
+    :domain => "www.traderline.pt",
+    :user_name => "newslettertest@traderline.pt",
+    :password => "news2013letter",
+    :authentication => :login,
+    :enable_starttls_auto => true
 }
 
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+
