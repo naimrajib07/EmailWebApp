2e2898ebad30b010258c492aa8b660cbad7b6a5c
Pandu Ramesh <bhanurorkitchen8@gmail.com>
2013-03-26 01:10:59 +0600

    - Added default from address for mailer class.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  #default from:  "naim@nascenia.com"
+  default from:  "test@emailwebapp.com"
 
   def send_mail(email)
     emailer_name = email.email_to.split("@")
