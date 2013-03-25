bd6dfea27affc3fb8aba039d2c58ec77dd7d4a4c
Pandu Ramesh <bhanurorkitchen8@gmail.com>
2013-03-26 02:45:26 +0600

    - Added mail sender username for sending email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def send_mail(email)
     emailer_name = email.email_to.split("@")
-    email_with_name = "#{emailer_name[0]} <#{email.email_to}>"
+    email_with_name = "'#{emailer_name[0]}' <#{email.email_to}>"
     attachments["#{email.email_attachment_file_name}"] = File.read(email.email_attachment.path) if email.email_attachment.present?
     @msg_body = email.email_body
     mail(:to =>"#{email_with_name}", :from => email.email_from, :bcc => email.bcc, :subject => email.email_subject)
