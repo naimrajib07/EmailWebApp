be03156d04a93f6b896725237a2fc90e4a86624a
Naim Rajib <naim_cse07@yahoo.com>
2013-04-13 11:15:32 +0600

    - Changed production host url according to heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ EmailWebApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'http://www.emailwebapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://emailwebapp.herokuapp.com' }
 
 end
